29960597a934d14ad2a40113e8b4d0d2acd3e584
samy bf <sbfadhloun@gmail.com>
2023-11-19 21:48:20 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,116 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+<h1>React training</h1>
+   <Navbar expand="lg" className="bg-body-tertiary">
+   <Container>
+     <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+     <Navbar.Collapse id="basic-navbar-nav">
+       <Nav className="me-auto">
+         <Nav.Link href="#home">Home</Nav.Link>
+         <Nav.Link href="#link">Link</Nav.Link>
+         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+           <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+           <NavDropdown.Item href="#action/3.2">
+             Another action
+           </NavDropdown.Item>
+           <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+           <NavDropdown.Divider />
+           <NavDropdown.Item href="#action/3.4">
+             Separated link
+           </NavDropdown.Item>
+         </NavDropdown>
+       </Nav>
+     </Navbar.Collapse>
+   </Container>
+ </Navbar> 
+ {/*first card*/}
+ 
+ <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>bootstrap project</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+{/* second card*/}
+
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Card number 2</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+    {/* third card*/}
+    <Card>
+      <Card.Body>Bonjour gomycode</Card.Body>
+    </Card>
+
+
+{/*form*/}
+
+<Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+    
+    
+    <Form.Group className="mb-3">
+    <Form.Label>Disabled input</Form.Label>
+    <Form.Control placeholder="Disabled input" disabled />
+  </Form.Group>
+  <Form.Group className="mb-3">
+    <Form.Label>Disabled select menu</Form.Label>
+    <Form.Select disabled>
+      <option>Disabled select</option>
+    </Form.Select>
+  </Form.Group>
+  <Form.Group className="mb-3">
+    <Form.Check type="checkbox" label="Can't check this" disabled />
+  </Form.Group>
+ 
+
+  <h2>bye bye</h2>
+
+  </>
   );
 }
-
 export default App;
